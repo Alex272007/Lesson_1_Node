c716ef5e76289b585688d1625a4a00aeba503d8f
Alex272007 <154067464+Alex272007@users.noreply.github.com>
2025-05-06 20:33:18 +0400

    add files

---
+++ Node_2_Seminar/calculateResultSum.js
@@ -0,0 +1,17 @@
+/*Задание №2
+1. Необходимо вынести функцию .calculateResultSum() в отдельный
+файл, импортировать её в основной файл и использовать.
+2. Также необходимо вынести запуск скрипта в скрипты запуска NPM,
+для того, чтобы можно было запускать скрипт с помощью команды
+npm run start.
+*/
+
+const np = require('number-precision');
+
+function calculateResultSum(purchases, discount) {
+    let total = purchases.reduce((acc, purchase) => np.plus(acc + purchase), 0);
+    total = np.times(total, discount);
+    return total;
+}
+
+module.exports = {calculateResultSum};
